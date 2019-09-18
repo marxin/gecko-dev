1d3d2fe242e439c1765aa2c54baaeb7bcc039cb5
Martin Liska <mliska@suse.cz>
2019-09-18 10:34:32 +0200

    Fix and enable PGO.

--- build/pgo/profileserver.py
+++ build/pgo/profileserver.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
     binary = runner_args.get('binary')
     if not binary:
-        binary = build.get_binary_path(where="staged-package")
+        binary = build.get_binary_path()
     binary = os.path.normpath(os.path.abspath(binary))
 
     path_mappings = {
